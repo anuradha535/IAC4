b1f4da14ca16235e364f004e6cb37612d9da7947
harsha <anuradha@hsbc.com>
2025-03-07 22:55:15 +0530

    changes done to c3

--- 1.tf
+++ 1.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc2" {
+    cidr_block = "10.0.0.0/24"
 	}
 	
---
+++ eip.tf
@@ -0,0 +1,3 @@
+resource "aws_eip" {
+    availability_zone="ap-southeast-1a"
+	}
